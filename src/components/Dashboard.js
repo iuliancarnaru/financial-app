e486b6a1b6d4ad5c3fcfa26d4e5901bb290a8183
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-04-18 13:46:53 +0100

    dashboard design

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,8 +5,14 @@ const Dashboard = props => {
   return (
     <header className="dashboard-container">
       <h1 className="heading-primary">Your stats for April</h1>
-      <h2 className="heading-secondary">Income:</h2>
-      <h2 className="heading-secondary">Outcome:</h2>
+      <h2 className="heading-secondary income">
+        Income: <span className="amount">+ 3400</span>
+        <span className="percentage">&nbsp;</span>
+      </h2>
+      <h2 className="heading-secondary outcome">
+        Outcome: <span className="amount">- 1900</span>
+        <span className="percentage">80%</span>
+      </h2>
     </header>
   );
 };
