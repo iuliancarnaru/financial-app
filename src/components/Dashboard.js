ec6cb49fa35460ca2fe0e1bb1c7e337c9829fb9b
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-04-18 17:26:00 +0100

    more style

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,40 +4,48 @@ import React from "react";
 const Dashboard = props => {
   return (
     <React.Fragment>
-      <header className="dashboard-container">
-        <h1 className="heading-primary">
-          <i class="icon ion-md-calendar margin-right-s" />
-          April
-        </h1>
-        <h2 className="heading-secondary income">
-          Income: <span className="amount">+ 3400</span>
-          <span className="percentage">&nbsp;</span>
-        </h2>
-        <h2 className="heading-secondary outcome">
-          Outcome: <span className="amount">- 1900</span>
-          <span className="percentage">80%</span>
-        </h2>
-      </header>
-      <section className="dashboard-card">
-        <div className="dashboard-card--home">
-          <h2>
-            <i class="icon ion-md-home" />
+      <div className="dashboard-main">
+        <section className="dashboard-container">
+          <h1 className="heading-primary">
+            <i class="icon ion-md-calendar margin-right-s" />
+            April
+          </h1>
+          <h2 className="heading-secondary income">
+            Income: <span className="amount">3400</span>
+            <span className="percentage">&nbsp;&nbsp; &nbsp;</span>
           </h2>
-          <h3>1200</h3>
-        </div>
-        <div className="dashboard-card--home">
-          <h2>
-            <i class="icon ion-md-pizza" />
+          <h2 className="heading-secondary outcome">
+            Outcome: <span className="amount">1900</span>
+            <span className="percentage">80%</span>
           </h2>
-          <h3>1200</h3>
-        </div>
-        <div className="dashboard-card--home">
-          <h2>
-            <i class="icon ion-md-bus" />
-          </h2>
-          <h3>1200</h3>
-        </div>
-      </section>
+        </section>
+
+        <section className="dashboard-card">
+          <div className="dashboard-card--home">
+            <i class="icon ion-md-home icon-color" />
+            <div className="flex-wrapper">
+              <div className="info-dot info-dot--bad">&nbsp;</div>
+              <p className="info-display">1650</p>
+            </div>
+          </div>
+
+          <div className="dashboard-card--home">
+            <i class="icon ion-md-pizza icon-color" />
+            <div className="flex-wrapper">
+              <div className="info-dot info-dot--medium">&nbsp;</div>
+              <p className="info-display">400</p>
+            </div>
+          </div>
+
+          <div className="dashboard-card--home">
+            <i class="icon ion-md-bus icon-color" />
+            <div className="flex-wrapper">
+              <div className="info-dot info-dot--good">&nbsp;</div>
+              <p className="info-display">120</p>
+            </div>
+          </div>
+        </section>
+      </div>
     </React.Fragment>
   );
 };
