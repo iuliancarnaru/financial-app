a6f93246dbebfe4d5ee0605636e23fa5ce88fdfd
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-04-18 14:14:08 +0100

    adding style

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,17 +3,42 @@ import React from "react";
 
 const Dashboard = props => {
   return (
-    <header className="dashboard-container">
-      <h1 className="heading-primary">Your stats for April</h1>
-      <h2 className="heading-secondary income">
-        Income: <span className="amount">+ 3400</span>
-        <span className="percentage">&nbsp;</span>
-      </h2>
-      <h2 className="heading-secondary outcome">
-        Outcome: <span className="amount">- 1900</span>
-        <span className="percentage">80%</span>
-      </h2>
-    </header>
+    <React.Fragment>
+      <header className="dashboard-container">
+        <h1 className="heading-primary">
+          <i class="icon ion-md-calendar margin-right-s" />
+          April
+        </h1>
+        <h2 className="heading-secondary income">
+          Income: <span className="amount">+ 3400</span>
+          <span className="percentage">&nbsp;</span>
+        </h2>
+        <h2 className="heading-secondary outcome">
+          Outcome: <span className="amount">- 1900</span>
+          <span className="percentage">80%</span>
+        </h2>
+      </header>
+      <section className="dashboard-card">
+        <div className="dashboard-card--home">
+          <h2>
+            <i class="icon ion-md-home" />
+          </h2>
+          <h3>1200</h3>
+        </div>
+        <div className="dashboard-card--home">
+          <h2>
+            <i class="icon ion-md-pizza" />
+          </h2>
+          <h3>1200</h3>
+        </div>
+        <div className="dashboard-card--home">
+          <h2>
+            <i class="icon ion-md-bus" />
+          </h2>
+          <h3>1200</h3>
+        </div>
+      </section>
+    </React.Fragment>
   );
 };
 
