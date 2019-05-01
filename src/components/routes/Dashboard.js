2c394c0a9bc40e839e86b9162c8e27078bb3c24e
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-05-01 16:25:09 +0100

    adding style for dashboard card

--- src/components/routes/Dashboard.js
+++ src/components/routes/Dashboard.js
@@ -1,26 +1,38 @@
 import React from "react";
 
 const Dashboard = () => {
+  const dashboardInfo = [
+    {
+      icon: "icon ion-ios-key",
+      amount: 1250,
+      color: "danger"
+    },
+    {
+      icon: "icon ion-ios-pizza",
+      amount: 400,
+      color: "neutral"
+    },
+    {
+      icon: "icon ion-ios-bus",
+      amount: 120,
+      color: "success"
+    }
+  ];
   return (
     <div className="dashboard">
-      <h1>Dashboard</h1>
-      <p>
-        Seamlessly provide access to transparent manufactured products after
-        adaptive leadership skills. Energistically maximize global vortals
-        vis-a-vis wireless intellectual capital. Quickly deliver prospective
-        functionalities whereas accurate technology. Monotonectally enable
-        tactical benefits vis-a-vis team building networks. Interactively
-        conceptualize error-free methodologies through distinctive
-        opportunities. Distinctively fabricate innovative materials rather than
-        just in time e-business. Uniquely extend technically sound
-        potentialities with wireless results. Assertively syndicate user
-        friendly quality vectors vis-a-vis viral sources. Assertively repurpose
-        proactive systems without fully tested e-tailers. Quickly pontificate
-        value-added outsourcing rather than orthogonal solutions. Monotonectally
-        myocardinate cutting-edge products through go forward processes.
-        Professionally initiate timely outsourcing via intuitive interfaces.
-        Authoritatively.
-      </p>
+      <div className="dashboard-general">
+        {dashboardInfo.map(item => (
+          <div key={item.icon} className="dashboard-general--card">
+            <i className={item.icon} />
+            <div className="price-container">
+              <span className={item.color} />
+              <p>{item.amount}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div>Part 2</div>
+      <div>Part 3</div>
     </div>
   );
 };
