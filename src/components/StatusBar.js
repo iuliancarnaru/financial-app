067a7e83fabfa37d86cb29f63284a984d85f2937
Iulian Carnaru <iulian.carnaru@immediate.co.uk>
2019-05-01 14:24:13 +0100

    handle online/offline

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const StatusBar = () => {
+  const [status, setStatus] = useState(navigator.onLine);
+
+  useEffect(() => {
+    window.addEventListener("online", handleOnline);
+    window.addEventListener("offline", handleOffline);
+  });
+
+  const handleOnline = () => {
+    setStatus(true);
+  };
+
+  const handleOffline = () => {
+    setStatus(false);
+  };
+
   return (
     <div className="status-bar flex-container-between ">
-      <div>Online</div>
+      <div>{status ? "Online" : "Ofline"}</div>
       <div>May 2019</div>
       <div>15 - Cloudy</div>
     </div>
